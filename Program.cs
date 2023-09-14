f9aaf73585101001ceaef3608d4c9a50e0149846
aasli <aasli@ASLINB>
2023-09-14 14:00:16 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string inputString = "Hello World and Coders"; // You can change this to any string
+        string reversedString = FirstReverse(inputString);
+        Console.WriteLine("Reversed String: " + reversedString);
+    }
+
+    static string FirstReverse(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
